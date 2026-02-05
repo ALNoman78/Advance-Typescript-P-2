76526a9190b0ca05eb64f247b9e46b5c7c381ab3
ALNoman78 <abdullaalno078@gmail.com>
2026-02-05 11:16:36 +0600

    class learning

--- src/class.ts
+++ src/class.ts
@@ -1,1 +1,16 @@
-// opp - class - object 
+// opp - class >>> to >>> object 
+
+class Animal {
+    name: string;
+    species: string;
+    sound: string;
+
+    constructor(name: string, species: string, sound: string) {
+        this.name = name;
+        this.sound = sound;
+        this.species = sound
+    }
+}
+
+const cat = new Animal("snow", "cat", "miywa")
+console.log(cat)
