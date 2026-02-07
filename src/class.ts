625704765504f52779d2ac0d65b3a654c3c1a902
ALNoman78 <abdullaalno078@gmail.com>
2026-02-07 10:35:12 +0600

    new method in

--- src/class.ts
+++ src/class.ts
@@ -1,40 +1,42 @@
-// opp - class >>> to >>> object 
+// opp - class >>> to >>> object
 
-class Animal {
-    name: string;
-    species: string;
-    sound: string;
+// class Animal {
+//     name: string;
+//     species: string;
+//     sound: string;
 
-    constructor(name: string, species: string, sound: string) {
-        this.name = name;
-        this.sound = sound;
-        this.species = sound
-    }
+//     constructor(name: string, species: string, sound: string) {
+//         this.name = name;
+//         this.sound = sound;
+//         this.species = sound
+//     }
 
-    makeSound() {
-        console.log(`The animal is make sound`)
-    }
-}
+//     makeSound() {
+//         console.log(`${this.name} is make sound : ${this.sound}`)
+//     }
+// }
 
-const cat = new Animal("snow", "cat", "miywa")
-// console.log(cat)
-console.log(cat.makeSound())
+// const cat = new Animal("Snow", "miyaw", "cat")
+// const dog = new Animal("dogeesh", "ghew ghew", "dog")
+
+
+
+// dog.makeSound()
+
+
 
 // todo this is all about the class and constructor
 
-class Bike {
-    model: string;
-    serial_number: string;
-    tank: string;
-    price: string;
+class vehicle {
 
-    constructor(model: string, serial_number: string, tank: string, price: string) {
-        this.model = model
-        this.serial_number = serial_number
-        this.tank = tank
-        this.price = price
+    constructor(public name: string, public engine: string, public topSpeed: string) {
+
     }
-    customizeBike() {
-        console.log("This is method ")
+    carSound() {
+        console.log(`${this.name} top speed is ${this.topSpeed}`)
     }
 }
+
+const car = new vehicle("Audi", "vet engine", "190k/m")
+
+console.log(car.name)
