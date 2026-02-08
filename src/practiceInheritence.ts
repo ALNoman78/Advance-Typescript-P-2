7cbe37f0fb8d1a5bc34646ae06476861be0ea92f
ALNoman78 <abdullaalno078@gmail.com>
2026-02-08 20:44:47 +0600

    global class added

---
+++ src/practiceInheritence.ts
@@ -0,0 +1,13 @@
+class Person {
+    name : string;
+    id : number;
+    address : string;
+    contactInfo : string;
+
+    constructor(name: string, id : number , address : string, contactInfo : string){
+        this.name = name
+        this.id = id
+        this.address = address
+        this.contactInfo = contactInfo
+    }
+}
