4bdeeb357e89adf13356f075d36a9b649df54f03
ALNoman78 <abdullaalno078@gmail.com>
2026-02-08 17:40:51 +0600

    inheritence completed with super method

--- src/inheritence.ts
+++ src/inheritence.ts
@@ -1,48 +1,62 @@
-class Students {
-    name : string;
-    id : number;
-    age : number;
-    address : string;
+class Person {
+    name: string;
+    id: number;
+    age: number;
+    address: string;
 
-    constructor(name: string, id : number , age : number , address : string){
+    constructor(name: string, id: number, age: number, address: string) {
         this.name = name
-        this. id = id
+        this.id = id
         this.age = age
         this.address = address
     }
 
-    getSleep(numberOfHours : number){
+    getSleep(numberOfHours: number) {
         console.log(`${this.name} sleep ${numberOfHours} hrs`)
     }
 }
 
-const student1 = new Students("Moule", 19, 23, "Mirpur 14")
-
-student1.getSleep(15);
 
 
-class Teacher {
-    name : string;
-    age : number;
-    address : string;
-    designation : string; // this one is extra property
 
-    constructor(name: string, age : number , address : string , designation: string){
-        this.name = name
-        this.age = age
-        this.address = address
+class Students extends Person {
+    rollNumber: number;
+
+    constructor(name: string, id: number, age: number, address: string, rollNumber: number) {
+
+        super(name, id, age, address)
+
+        this.rollNumber = rollNumber
+
+    }
+}
+
+const student1 = new Students("Moule", 19, 23, "Mirpur 14", 202630019)
+
+console.log(student1.rollNumber)
+
+
+
+
+class Teacher extends Person {
+
+    designation: string; // own property
+
+    constructor(name: string, id: number, age: number, address: string, designation: string) {
+
+
+        // todo If i use Person class constructor then I've to use ** super() ** method
+
+        super(name, id, age, address)
+
         this.designation = designation
     }
 
-    getSleep(numberOfHours : number){
-        console.log(`${this.name} sleep ${numberOfHours} hrs`)
-    }
-
-    takeClass(){
-        console.log(`${this.name} take hrs of class`)
+    takeClass(numOfClass: number) {
+        console.log(`${this.name} take ${numOfClass} hrs of class`)
     }
 }
 
-const teacher1 = new Teacher ('Noman', 22 , 'Mirpur-13','Senior Teacher' )
+const teacher1 = new Teacher('Noman', 16306, 22, 'Mirpur-13', 'Senior Teacher')
 
-console.log(teacher1.takeClass())
+teacher1.address
